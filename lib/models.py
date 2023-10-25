793f7a95d2dfd91e66f9eae3b604df0a94521e88
Mary <omota4fidelity@gmail.com>
2023-10-25 02:01:20 +0100

    commit

--- lib/models.py
+++ lib/models.py
@@ -2,8 +2,8 @@
 
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, create_engine, ForeignKey
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
 
 
 
@@ -20,14 +20,9 @@ class Owner(Base):
 
     cars = relationship('Car', back_populates='owner')
 
-    def __init__(self, first_name, last_name, phone_no):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_no = phone_no
-
+   
     def __repr__(self):
-        return f"{self.id}, " + \
-            f"{self.first_name}, " + \
+        return f"{self.first_name}, " + \
             f"{self.last_name}, " + \
             f"{self.phone_no}"
     
@@ -53,50 +48,17 @@ class Car(Base):
         
     owner = relationship('Owner', back_populates='cars')
 
-
-    def __init__(self, car_brand, color, production_year, plate_no, owner_id):
-        self.car_brand = car_brand
-        self.color = color
-        self.production_year = production_year
-        self.plate_no = plate_no
-        self.owner_id = owner_id
-        
-
     def __repr__(self):
-        return f"{self.id}, " + \
-            f"Car Brand: {self.car_brand}, Car color:{self.color}" + \
+        return f"Car Brand: {self.car_brand}, Car color: {self.color} " + \
             f"Year: {self.production_year}, Plate no:{self.plate_no}" + \
             f"{self.owner_id}"
-
-
-
-# owner1 = Owner(
-#     first_name="Mary", last_name="MaryMAry",
-#     phone_no=123456789
-# )
-
-# owner2 = Owner(
-#     first_name="Owen", last_name="Oweeeeen",
-#     phone_no=23456789
-# )
-
-# owner3 = Owner(
-#     first_name="Moses", last_name="Oyeeee",
-#     phone_no=45637255
-# )
-# owner4 = Owner(
-#     first_name="Johnny", last_name="Ola",
-#     phone_no=123456789
-# )
-# owner5 = Owner(
-#     first_name="Feyi", last_name="Tee",
-#     phone_no=12345678
-# )
-
 
 
 engine = create_engine("sqlite:///data.db", echo=True)
 Base.metadata.create_all(bind=engine)
 
-# Session = sessionmaker(bind=engine)
-# session = Session()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+
--- lib/mycalc.py
+++ lib/mycalc.py
@@ -1,11 +1,35 @@
 import click
+from models import Owner, Car, session, Base, engine
 
 @click.command()
-@click.option('--number', '-n')
+@click.option('--first_name', prompt='Enter First Name')
+@click.option('--last_name', prompt='Enter Last Name')
+@click.option('--phone_no', prompt='Phone Number')
+@click.option('--car_brand', prompt='Enter Car Brand')
+@click.option('--color', prompt='Enter Car Color')
+@click.option('--production_year', prompt='Enter Car Production Year')
+@click.option('--plate_no', prompt='Enter Car Plate no')
+@click.option('--owner_id', prompt='Enter Owner ID')
 
-def main(number):
-    click.echo(f"Your number is {number}")
+def owner_reg(first_name,last_name,phone_no, car_brand, color, production_year, plate_no, owner_id):
+    customer = Owner(
+        first_name = first_name,
+        last_name = last_name,
+        phone_no = phone_no
+    )
+    customer_car = Car(
+        car_brand = car_brand,
+        color = color,
+        production_year = production_year,
+        plate_no = plate_no,
+        owner_id = owner_id       
+    )
+
+    session.add(customer)
+    session.add(customer_car)
+    session.commit()
 
 
 if __name__=="__main__":
-    main()
+    owner_reg()
+    
